f5ef1d313870b2981988a957a79a028555deb945
AD\MAEGAWA_S <216467785+shuta-maegawa@users.noreply.github.com>
2025-10-01 17:22:04 +0900

    chore: 一時的なコメントを追加

--- a.ts
+++ a.ts
@@ -17,3 +17,5 @@ console.log(evenNumbers); // Output: [2, 4]
 // map array to double each number
 export const doubledNumbers = numbers.map((num) => num * 2);
 console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+//tmp
